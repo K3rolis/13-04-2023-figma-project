082a7bf92665a5212e5225d65cda2bc2f63705bc
K3rolis <karolis.sivickas@gmail.com>
2023-04-16 18:55:11 +0300

    added dark mode

---
+++ script.js
@@ -0,0 +1,13 @@
+const toggleSwitch = document.querySelector("#theme-switch-input");
+
+function switchTheme(event) {
+  console.log(event.target.checked);
+
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme);
